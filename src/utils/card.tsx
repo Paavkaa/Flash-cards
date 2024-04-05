7629e060cac710e431f5d9ec84f2098218f0f0a7
Paavkaa <terezapavelkova1@gmail.com>
2024-04-05 17:22:32 +0200

    Started creating rotation of card based on mouse position

--- src/utils/card.tsx
+++ src/utils/card.tsx
@@ -4,11 +4,33 @@ import './styles.css'
 
 export default function Card() {
     // State to track whether the card is flipped
-    const [isFlipped, setIsFlipped] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false)
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    // Function to track mouse position
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        const { left, top } = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - left;
+        const y = e.clientY - top;
+
+        setMousePosition({ x, y });
+
+        console.log(mousePosition);
+    };
+
+    const calculateRotation = (x: number, y: number, cardElement: HTMLDivElement) => {
+        const cardRect = cardElement.getBoundingClientRect();
+        const cardWidth = cardRect.width;
+        const cardHeight = cardRect.height;
+        const tiltAngle = 10;
+        const angleX = (cardHeight / 2 - y) / cardHeight * tiltAngle;
+        const angleY = (x - cardWidth / 2) / cardWidth * tiltAngle;
+
+        return `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+    }
 
     // Spring animation definitions
     const { transform, /*opacity*/ } = useSpring({
-        /*opacity: isFlipped ? 1 : 0,*/
         transform: `perspective(600px) rotateY(${isFlipped ? 180 : 0}deg)`,
         config: { mass: 5, tension: 500, friction: 80 },
     });
@@ -19,8 +41,12 @@ export default function Card() {
     return (
         <animated.div
             className="Card"
-            style={{ transform, cursor: 'pointer' }}
+            style={{
+                transform,
+                cursor: 'pointer',
+            }}
             onClick={toggleFlip} // Add click event to toggle the flip state
+            onMouseMove={handleMouseMove} // Add mouse move event to track the mouse position
         >
             {
                 isFlipped ? (
