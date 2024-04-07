ea41d213fc3162d7a2f99b30fd0e545e872be04d
Paavkaa <terezapavelkova1@gmail.com>
2024-04-07 18:31:28 +0200

    Styling of home page

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <div className="Main">
             <FloatingLines/>
             <Nav/>
+
             <div>
                 <h1 className="Title">FusionCards</h1>
                 <h2 className="Subtitle">Learn fast way with flash cards</h2>
@@ -24,13 +25,19 @@ export default function Home() {
                 </button>
             </div>
 
-            <div className="DivBackground">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis commodi corporis dolorem
-                ducimus expedita fugit, iste neque nesciunt porro repellendus veniam. Consectetur ipsa, iste natus
-                numquam vel veniam voluptatum!
+            <div className="Flex JustifyCenter">
+                <div className="PreviewCard">
+                    <Card/>
+                </div>
+
+                <div className="DivBackground Width50">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis commodi corporis
+                    dolorem
+                    ducimus expedita fugit, iste neque nesciunt porro repellendus veniam. Consectetur ipsa, iste natus
+                    numquam vel veniam voluptatum!
+                </div>
             </div>
 
-            <Card />
         </div>
     );
 }
